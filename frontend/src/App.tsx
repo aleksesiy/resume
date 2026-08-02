8b41bcbf8e7e250db122d4d3fc07db5324156d20
aleksey <lexa.svsk@gmail.com>
2026-08-02 14:23:06 +0700

    feat: add process section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import Hero from "./sections/Hero";
 import Philosophy from "./sections/Philosophy";
+import Process from "./sections/Process";
 
 function App() {
   return (
     <>
       <Hero />
       <Philosophy />
+      <Process />
     </>
   );
 }
